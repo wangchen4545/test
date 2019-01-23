6863fae5e61ae41b50398f59826010fa9521b04e
王晨 <wangchen@MacBook-Pro-wangchen.local>
2019-01-23 10:10:16 +0800

    updata

---
+++ testReactNative/index.ios.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Home from './view/home'
+export default class testReactNative extends Component {
+  render() {
+    return (
+      <Home></Home>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('testReactNative', () => testReactNative);
---
+++ testReactNative/view/title.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+  } from 'react-native';
+export default class Home extends Component {
+    render() {
+      return (
+        <Text>
+          ccccs
+        </Text>
+      );
+    }
+  }
+  
